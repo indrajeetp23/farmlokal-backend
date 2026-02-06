8a8f66200f9cf71c7521a9d25ce6e775e76bfd7e
indrajeet patel <indrajeetpatel2308@gmail.com>
2026-02-06 23:57:36 +0530

    updated server.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import app from "./app.js";
 import connectDB from "./config/db.js";
-
 import { ENV } from "./config/env.js";
+
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(ENV.PORT, () => {
-  console.log(`🚀 Server running on port ${ENV.PORT}`);
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
+
 
